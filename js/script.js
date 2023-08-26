58fe7691b2470496c2522b3689760be04a2d5df5
Tamirat <abc@gmail.com>
2023-08-26 10:38:07 +0300

    local storage is added

--- js/script.js
+++ js/script.js
@@ -1,61 +1,86 @@
 const insertText = document.querySelector('.insert--text')
 const saveBtn = document.querySelector('.save')
-const listItem = document.querySelector('.list')
-
 const listWrapper = document.querySelector('.list-wrapper')
-
 const ITEMS = []
+
 var list
 saveBtn.addEventListener('click', () => {
     if (insertText.value) {
         ITEMS.push(insertText.value)
-        createList(ITEMS)
-        
+        createList(ITEMS,insertText.value )
     }
     insertText.value = ''
     
     
+    
 })
 
-console.log(ITEMS)
 // create list items function
-const createList = (ITEMS) => {
-    if (ITEMS.length === 0) {
-        list = `<ul class="list">
-            <li class = "title--list"> No things to do </li>
-        </ul>`
-        listWrapper.innerHTML = list
+const createList = (ITEMS, enteredValue) => {
+    if (ITEMS.length === 0 || localStorage.getItem('data')=== '') {
+        list =document.createElement('li')
+        list.classList.add('title--list')
+        list.innerHTML = "No things to do"
+        listWrapper.appendChild(list)
+
     }
     else  {
         if (ITEMS.length === 1) {
-            ITEMS.forEach(element => {
-                list = `<ul class = "list" > 
-            <li class="title--list"> ${element} <i class = "delete fa fa-trash" aria - hidden = "true" > </i> </li>
-            </ul >
-            `
-                listWrapper.innerHTML = list
-            }
-            )
-        } else {
             listWrapper.innerHTML = ''
-            ITEMS.forEach(element => {
-                list = `<ul class = "list" > 
-            <li class="title--list"> ${element} <i class = "delete fa fa-trash" aria - hidden = "true" > </i> </li>
-            </ul >
-            `
-                listWrapper.innerHTML += list
-            }
-            )
-
+            list = document.createElement('li')
+            list.classList.add('title--list')
+            list.innerHTML = enteredValue
+            let i = document.createElement('i')
+            const classes = ['delete', 'fa', 'fa-trash']
+            classes.forEach((cls) => {
+                i.classList.add(cls)
+            })
             
+            list.appendChild(i)
+            listWrapper.appendChild(list) 
+            saveData()
+        } else {
+            list = document.createElement('li')
+            list.classList.add('title--list')
+            list.innerHTML = enteredValue
+            let i = document.createElement('i')
+            const classes = ['delete', 'fa', 'fa-trash']
+            classes.forEach((cls) => {
+                i.classList.add(cls)
+            })
             
+            list.appendChild(i)
+            listWrapper.appendChild(list)
+            saveData()
+       
         }
     };
     
 }
 
 createList(ITEMS)
+// delete and line-through functionality
+listWrapper.addEventListener('click', (e) => {
+    if (e.srcElement.classList[0] === 'delete') {
+        console.log(e.target.parentElement.remove())
+        saveData()
+       
+    }
+    else if (e.srcElement.classList[0] === 'title--list') {
+        e.srcElement.classList.toggle('overwrite')
+        saveData()
+    }
+    
+    // console.log(e.srcElement.classList)
+})
 
 
 
-
+// save data on local storage
+function saveData() {
+    localStorage.setItem('data', listWrapper.innerHTML)
+}
+function getData() {
+    listWrapper.innerHTML = localStorage.getItem('data')
+}
+getData()
