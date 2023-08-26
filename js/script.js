b88366a1488a07521fa6e2dd49cb5edf347e8789
Tamirat <abc@gmail.com>
2023-08-26 11:15:08 +0300

    error fixed

--- js/script.js
+++ js/script.js
@@ -17,46 +17,31 @@ saveBtn.addEventListener('click', () => {
 
 // create list items function
 const createList = (ITEMS, enteredValue) => {
-    if (ITEMS.length === 0 || localStorage.getItem('data')=== '') {
-        list =document.createElement('li')
-        list.classList.add('title--list')
-        list.innerHTML = "No things to do"
-        listWrapper.appendChild(list)
-
-    }
-    else  {
-        if (ITEMS.length === 1) {
-            listWrapper.innerHTML = ''
-            list = document.createElement('li')
-            list.classList.add('title--list')
-            list.innerHTML = enteredValue
-            let i = document.createElement('i')
-            const classes = ['delete', 'fa', 'fa-trash']
-            classes.forEach((cls) => {
-                i.classList.add(cls)
-            })
-            
-            list.appendChild(i)
-            listWrapper.appendChild(list) 
-            saveData()
-        } else {
-            list = document.createElement('li')
-            list.classList.add('title--list')
-            list.innerHTML = enteredValue
-            let i = document.createElement('i')
-            const classes = ['delete', 'fa', 'fa-trash']
-            classes.forEach((cls) => {
-                i.classList.add(cls)
-            })
-            
-            list.appendChild(i)
-            listWrapper.appendChild(list)
-            saveData()
-       
+    list = document.createElement('li')
+    list.classList.add('title--list')
+    if (enteredValue === undefined) {
+        list.innerHTML = "Click delete to delete"
+        
+    } else {
+        if (list.innerHTML.includes("NO THING FOUND")) {
+            console.log(true)
+            list.parentElement.remove()
         }
-    };
+        list.innerHTML = enteredValue
+    }
+    let i = document.createElement('i')
+    const classes = ['delete', 'fa', 'fa-trash']
+    classes.forEach((cls) => {
+        i.classList.add(cls)
+    })
     
-}
+    list.appendChild(i)
+    listWrapper.appendChild(list) 
+    saveData()
+    
+};
+    
+
 
 createList(ITEMS)
 // delete and line-through functionality
@@ -80,6 +65,7 @@ listWrapper.addEventListener('click', (e) => {
 function saveData() {
     localStorage.setItem('data', listWrapper.innerHTML)
 }
+
 function getData() {
     listWrapper.innerHTML = localStorage.getItem('data')
 }
